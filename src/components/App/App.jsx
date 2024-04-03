a7662784fe45e7d4c56376fcbd6fe4575f5029df
laposhko <laposhko0@gmail.com>
2024-04-03 19:05:24 +0300

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,23 +2,23 @@ import Description from "../Description/Description";
 import Options from "../Options/Options";
 import Feedback from "../Feedback/Feedback";
 import Notification from "../Notification/Notification";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  if (!localStorage.getItem("counts")) {
-    localStorage.setItem(
-      "counts",
-      JSON.stringify({
-        good: 0,
-        neutral: 0,
-        bad: 0,
-      })
-    );
-  }
-  const [counts, setCounts] = useState(
-    JSON.parse(localStorage.getItem("counts"))
-  );
-  localStorage.setItem("counts", JSON.stringify(counts));
+  const [counts, setCounts] = useState(() => {
+    const savedData = JSON.parse(localStorage.getItem("feedback"));
+    if (savedData !== null) {
+      return savedData;
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(counts));
+  }, [counts]);
   function updateFeedback(feedbackType) {
     setCounts({ ...counts, [feedbackType]: counts[feedbackType] + 1 });
   }
